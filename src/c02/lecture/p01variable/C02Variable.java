4d38759f1dbf0aa73d6f62aa2f8dd583c7a36dcd
jung tae wook <xodnr901901@naver.com>
2024-02-19 16:02:30 +0900

    변수와 타입

---
+++ src/c02/lecture/p01variable/C02Variable.java
@@ -0,0 +1,12 @@
+package c02.lecture.p01variable;
+
+public class C02Variable {
+    public static void main(String[] args) {
+        //변수에 값이 할당되지 않았을떄 사용할수 없음
+        // 변수 선언과 값 대입을 한명령문에 할수있음
+        //변수  여러개를 한 명령문에 선언가능
+        //여러 변수 선언 값 할당을 한명령문에서 작성가능
+        int g=11, k=8 ,p=30;
+
+    }
+}
---
+++ src/c02/lecture/p02type/C02Integer.java
@@ -0,0 +1,11 @@
+package c02.lecture.p02type;
+
+public class C02Integer {
+    public static void main(String[] args) {
+        // int 최대 2_147_483_647 +- ;
+        /* long (int 최소값 최대값넘어가실)
+         long  2_147_483_648L 붙여줘야됨
+         long  2_147_483_647작아도 l 붙여되기함*/
+
+    }
+}
