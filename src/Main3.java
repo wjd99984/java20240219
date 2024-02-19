0362dbec184e96990509abccee9e6e45adc0ef32
jung tae wook <xodnr901901@naver.com>
2024-02-19 12:41:32 +0900

    처음 만들어본 코드

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("고양이");
+        System.out.println("고양이 ");
+        System.out.println("나만없어 ");
+        System.out.println("고야잉");
+        System.out.println("고양이");
+    }
+}
