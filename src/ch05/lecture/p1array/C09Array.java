5ce9ff5b05f09d4b6f5fcd5917f1ec6d540c1d5d
jung tae wook <xodnr901901@naver.com>
2024-02-27 16:50:37 +0900

    02 27-1

---
+++ src/ch05/lecture/p1array/C09Array.java
@@ -0,0 +1,16 @@
+package ch05.lecture.p1array;
+
+public class C09Array {
+    public static void main(String[] args) {
+        int[] arr1;
+        arr1 = new int[]{5, 3};
+        int[] arr2 = new int[]{6, 5};
+        //배열의 선언과 할당을 동시에 할때 new int[] 생략가능
+        int[] arr3 = {8, 3, 2};
+
+        int[] arr4;
+        //arr4 = {4, 2, 1};//x
+
+        var arr5 = new int[]{10, 11, 12};
+    }
+}
